ad929c7b5ca9be03e809414bd46101c333ca4ada
ESAScanada <shawn@esportsarena.com>
2023-01-11 05:23:11 -0500

    changing index to php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Main Index.html -->
+<?php include 'header.php'; ?>
+<!-- Main Content -->
+<div class="main-content">
+  <?php include 'navbar.php'; ?>
+  <div class="content">
+    <p>Welcome to my website!</p>
+  </div>
+  <?php include 'sidebar.php'; ?>
+</div>
+<!-- Main Content -->
+<?php include 'footer.php'; ?>
